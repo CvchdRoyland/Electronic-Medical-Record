38983bad37fa4d56dcdd170c1ca92a9ea8d3f070
Royland Badua <cvchdictu.royland@gmail.com>
2025-02-14 19:23:11 +0800

    added UserSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -25,6 +25,7 @@ class DatabaseSeeder extends Seeder
            DemographicSeeder::class,
            IpGroupSeeder::class,
            RelationshipSeeder::class,
+           UserSeeder::class,
         ]);
     }
 }
